4a3d39bb2ade91f544350036c4ae16d73317aaa9
Alireza <103531312+alirezashon@users.noreply.github.com>
2024-01-28 19:26:52 +0330

    manage socket

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,44 +1,77 @@
-// src/websocket/websocket.gateway.ts
-
+import { Logger } from '@nestjs/common';
 import {
-  WebSocketGateway as GatewayDecorator,
-  WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WsResponse,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Socket } from 'socket.io';
 
-@GatewayDecorator() 
-export class MyWebSocketGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
-{
-  @WebSocketServer() server: Server;
-
-  private onlineUsersCount = 0;
-  private maxScrapingConnections = 10;
-
-  handleConnection(client: any, ...args: any[]) {
-    console.log('Client connected');
-
-    this.onlineUsersCount++;
-
-    if (this.onlineUsersCount <= this.maxScrapingConnections) {
-      client.on('startScraping', (data) => {
-        console.log('Start scraping:', data);
-        // Implement your scraping logic here
-
-        // Example: Send a message back to the client
-        client.emit('scrapingStatus', { status: 'Scraping in progress' });
-      });
-    } else {
-      client.emit('maxScrapingConnectionsReached', {
-        message: 'Maximum scraping connections reached',
-      });
-    }
+@WebSocketGateway()
+export class AppGateway implements OnGatewayInit {
+  private logger: Logger = new Logger(' Socketalistasiti');
+  afterInit(server: any) {
+    this.logger.log('inaliziazed');
   }
 
-  handleDisconnect(client: any) {
-    console.log('Client disconnected');
-    this.onlineUsersCount--;
+  handleDisconnect(client: Socket) {
+    this.logger.log(
+      'web socket gheyre connect mibashad lotgfan ba osdoem admin tamas hasel farmaiid',
+      client.id,
+    );
+  }
+  handleConnection(client:Socket, ...args:any[]) {
+    this.logger.log('client connected' , client.id)
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    // client.emit('message', text)
+    return { event: `msg to client is `, data: text };
   }
 }
+
+// // src/websocket/websocket.gateway.ts
+
+// import {
+//   WebSocketGateway as GatewayDecorator,
+//   WebSocketServer,
+//   OnGatewayConnection,
+//   OnGatewayDisconnect,
+// } from '@nestjs/websockets';
+// import { Server } from 'socket.io';
+
+// @GatewayDecorator() // Rename the decorator to avoid conflict
+// export class MyWebSocketGateway
+//   implements OnGatewayConnection, OnGatewayDisconnect
+// {
+//   @WebSocketServer() server: Server;
+
+//   private onlineUsersCount = 0;
+//   private maxScrapingConnections = 10;
+
+//   handleConnection(client: any, ...args: any[]) {
+//     console.log('Client connected');
+
+//     this.onlineUsersCount++;
+
+//     if (this.onlineUsersCount <= this.maxScrapingConnections) {
+//       client.on('startScraping', (data) => {
+//         console.log('Start scraping:', data);
+//         // Implement your scraping logic here
+
+//         // Example: Send a message back to the client
+//         client.emit('scrapingStatus', { status: 'Scraping in progress' });
+//       });
+//     } else {
+//       client.emit('maxScrapingConnectionsReached', {
+//         message: 'Maximum scraping connections reached',
+//       });
+//     }
+//   }
+
+//   handleDisconnect(client: any) {
+//     console.log('Client disconnected');
+//     this.onlineUsersCount--;
+//   }
+// }
